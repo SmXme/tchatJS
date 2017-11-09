e4cf4d80f8098877d4643f900e0a756c00e73619
SmXme <pillot.clement@gmail.com>
2017-11-09 15:36:45 +0100

    Created the getUsers function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 function createUser(){
-
-	$.ajax("http://messenger.api.niamor.com/createUser").done(function(mesData){
-		console.log(mesData);
+	$.ajax("http://messenger.api.niamor.com/createUser").done(function(myUser){
+		console.log("Voici mon user :");
+		console.log(myUser);
 	});
 }
-
 $(document).ready(createUser);
+
+function getUsers(){
+	$.ajax("http://messenger.api.niamor.com/getUsers").done(function(myUsers){
+		console.log("Voici mes users connectés :");
+		console.log(myUsers);
+	});
+}
+$(document).ready(getUsers);

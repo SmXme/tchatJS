661dd07077a37a98b8695efb90b84e8e815e02e3
SmXme <pillot.clement@gmail.com>
2017-11-13 10:01:07 +0100

    Created the getMessages function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 myself=0;
+	function getMessages(){
+
+	parameters = {
+					url: 'http://messenger.api.niamor.com/getMessages',
+					method: "post",
+					data: {
+						authKey : myself.authKey,
+						lastId : 0
+					}
+			};
+
+
+	$.ajax(parameters).done(function(messages){
+		console.log(messages);
+	});
+}
+
 function createUser(){
 	$.ajax("http://messenger.api.niamor.com/createUser").done(function(myUser){
 		myself = myUser;
 		console.log(myself.authKey);
+		getMessages();
 	});
 }
 
